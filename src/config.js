57d29d7f48abc09b64a4a58aa37b8be2c5cb934d
rahmor <rahmor@yahoo.com>
2019-06-03 13:44:12 -0400

    errors

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL,
+  DATABASE_URL: process.env.DATABASE_URL,
   TEST_DB_URL: process.env.TEST_DB_URL,
-  JWT_SECRET: process.env.JWT_SECRET,
-  DATABASE_URL: process.env.DATABASE_URL
+  JWT_SECRET: process.env.JWT_SECRET
+  // DATABASE_URL: process.env.DATABASE_URL
 };
