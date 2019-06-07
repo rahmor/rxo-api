bbf6d41809d01bb98277b28c1ebc75f8c407a277
rahmor <rahmor@yahoo.com>
2019-06-07 15:33:31 -0400

    removed time from add prescription endpoint

--- src/prescriptions/prescriptions-router.js
+++ src/prescriptions/prescriptions-router.js
@@ -5,6 +5,8 @@ const jsonBodyParser = express.json();
 const PrescriptionsService = require('./prescriptions-service');
 const { requireAuth, getIdFromToken } = require('../middleware/jwt-service');
 
+//get id from token, if id in token matches user requested if/ true send data, false send
+//response unauthized for this data.
 prescriptionsRouter
   .route('/:id')
   .all(requireAuth)
@@ -18,9 +20,9 @@ prescriptionsRouter
   .route('/')
   .all(requireAuth)
   .post(jsonBodyParser, (req, res) => {
-    const { rx_name, day, time } = req.body;
+    const { rx_name, day } = req.body;
     const user_id = getIdFromToken(req.get('Authorization'));
-    const prescription = { rx_name, day, time, user_id };
+    const prescription = { rx_name, day, user_id };
     PrescriptionsService.addUserPrescriptions(
       req.app.get('db'),
       prescription
